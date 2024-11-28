f0857b6d9c47dbdd87a118b67b9690df391714a0
Baxa-1998 <baxtiyornarziev6@gmail.com>
2024-11-28 12:25:46 +0500

    new

--- components/shared/Stages/Stages.jsx
+++ components/shared/Stages/Stages.jsx
@@ -10,37 +10,44 @@ export default function Stages() {
   return (
     <section ref={stagesRef} className="stages">
       <div className="stages__wrapper">
-      <TimelineContent animationNum={1} timelineRef={stagesRef}>
+   
       <div className="stages__items">
-          <h2>01</h2>
+      <TimelineContent animationNum={1} timelineRef={stagesRef}>
+      <h2>01</h2>
           <h3>Весь материал разбит  на небольшие уроки</h3>
           <p>
             Теория и практика подаются <br /> маленькими порциями. Так <br /> вам будет легче усваивать <br /> новые
             знания.
           </p>
-        </div>
       </TimelineContent>
+       
+        </div>
+      
 
 
-      <TimelineContent animationNum={2} timelineRef={stagesRef}>
+   
       <div className="stages__items">
+      <TimelineContent animationNum={2} timelineRef={stagesRef}>
           <h2>02</h2>
           <h3>Обучение через практику</h3>
           <p>
           Всё, что вы узнали, вы тут же начинаете применять на практике. Вы сразу видите результаты своего труда.
           </p>
+          </TimelineContent>
         </div>
-      </TimelineContent>
-      <TimelineContent animationNum={3} timelineRef={stagesRef}>
+    
+    
       <div className="stages__items">
+      <TimelineContent animationNum={3} timelineRef={stagesRef}>
           <h2>03</h2>
           <h3>Нет ограничений по времени</h3>
           <p>
           Можно совмещать учёбу с работой 
           и другими делами. Не нужно выпрашивать академический отпуск, если пришлось сделать перерыв.
           </p>
+          </TimelineContent>
         </div>
-      </TimelineContent>
+    
       
       </div>
       <div className="btn-wrapper">
